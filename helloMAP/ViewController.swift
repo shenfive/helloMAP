b32743a43a25eb545689294a3362a68f5baa6a6c
Danny Shen <shenfive@gmail.com>
2019-12-28 16:33:55 +0800

    init

--- helloMAP/ViewController.swift
+++ helloMAP/ViewController.swift
@@ -7,9 +7,11 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mymap: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
