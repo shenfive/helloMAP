6c9008dea8e954395416136ce0ed254f31d6cb2e
Danny Shen <shenfive@gmail.com>
2019-12-28 17:01:51 +0800

    ann

--- helloMAP/ViewController.swift
+++ helloMAP/ViewController.swift
@@ -14,6 +14,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var mymap: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        mymap.isScrollEnabled = false
+        mymap.isZoomEnabled = false
+        mymap.isRotateEnabled = true
         DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
             let latitude:CLLocationDegrees = 25.0444032
             let longitude:CLLocationDegrees = 121.5141468
@@ -23,7 +26,15 @@ class ViewController: UIViewController {
             let span:MKCoordinateSpan = MKCoordinateSpan(latitudeDelta: yScale, longitudeDelta: xScale)
             let region:MKCoordinateRegion = MKCoordinateRegion.init(center: location, span: span)
             self.mymap.setRegion(region, animated: true)
-
+            DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                let annotation = MKPointAnnotation()
+                annotation.coordinate = location
+                annotation.title = "譯智"
+                annotation.subtitle = "教育訓練中心"
+                self.mymap.addAnnotation(annotation)
+            }
+            
+            
         }
         
     }
